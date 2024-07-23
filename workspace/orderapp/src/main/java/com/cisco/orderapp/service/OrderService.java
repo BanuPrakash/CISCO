141f8bc51bd888b7eda3fb440416bd8ba8a68045
Banuprakash <banuprakashc@yahoo.co.in>
2024-07-23 15:10:38 +0530

    day 2

--- workspace/orderapp/src/main/java/com/cisco/orderapp/service/OrderService.java
+++ workspace/orderapp/src/main/java/com/cisco/orderapp/service/OrderService.java
@@ -5,17 +5,24 @@ import com.cisco.orderapp.dao.ProductDao;
 import com.cisco.orderapp.entity.Customer;
 import com.cisco.orderapp.entity.Product;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderService {
-    // Spring Data JPA generates classes for the JpaRepository interface
-    // which is wired
-    private final ProductDao productDao; // Constructor DI instead of @Autowired
-    private final CustomerDao customerDao;  // Constructor DI
+    @Autowired
+    private ProductDao productDao;
+
+    @Autowired
+    private CustomerDao customerDao;
+    
+//    // Spring Data JPA generates classes for the JpaRepository interface
+//    // which is wired
+//    private final ProductDao productDao; // Constructor DI instead of @Autowired
+//    private final CustomerDao customerDao;  // Constructor DI
 
     public List<Product> getProducts() {
         return productDao.findAll();
