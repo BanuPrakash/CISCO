4055634e394652b2fccb2c389f706e683c060079
Banuprakash <banuprakashc@yahoo.co.in>
2024-07-23 11:32:49 +0530

    day 2

--- workspace/demo/src/main/java/com/cisco/demo/DemoApplication.java
+++ workspace/demo/src/main/java/com/cisco/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.cisco.demo;
 
+import com.cisco.demo.entity.Employee;
+import com.cisco.demo.service.AppService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,6 +11,17 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+		String[] beans = ctx.getBeanDefinitionNames();
+		for(String bean: beans) {
+			System.out.println(bean);
+		}
+
+		System.out.println("******");
+
+		AppService service = ctx.getBean("appService", AppService.class);
+		Employee e = new Employee("Roger", "roger@cisco.com");
+		service.insert(e);
 	}
 
 }
